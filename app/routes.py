c706b2400c0da4de4286cdf3308f7658406b575a
EC2 Default User <ec2-user@ip-172-31-36-7.us-east-2.compute.internal>
2019-08-02 14:20:53 +0000

    login and signup

--- app/routes.py
+++ app/routes.py
@@ -2,41 +2,55 @@ import os
 from app import app
 from flask import render_template, request, redirect
 
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
+from flask_pymongo import PyMongo
 
-
-# from flask_pymongo import PyMongo
+app.secret_key = b'\x1b\xe3)\x89Hz\x83X\xb6\xe2\xdb\x17\xa1J\x94J'
 
 # name of database
-# app.config['MONGO_DBNAME'] = 'database-name' 
+app.config['MONGO_DBNAME'] = 'database' 
 
 # URI of database
-# app.config['MONGO_URI'] = 'mongo-uri' 
+app.config['MONGO_URI'] = 'mongodb+srv://admin:Purple123@cluster0-3obbt.mongodb.net/database?retryWrites=true&w=majority' 
 
-# mongo = PyMongo(app)
-
-
-# INDEX
+mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return render_template('index.html', events = events)
+    return render_template('index.html')
 
+@app.route('/signup', methods=['GET','POST'])
 
-# CONNECT TO DB, ADD DATA
+def signup():
+    if request.method == 'POST':
+        #take in info, check if username is taken, if it is available, put in database of users
+        users = mongo.db.project_users
+        existing_user = users.find_one({"username":request.form['username']})
+        if existing_user is None:
+            users.insert({"username":request.form['username'], "password":request.form['password']})
+            return "User saved"
+        else:
+            message = "That username is taken. Try logging in or try a different username."
+            return render_template('signup.html', message = message)
+    else:
+        return render_template('signup.html', message = '')
+        
+@app.route('/login', methods= ["POST", "GET"])
 
-@app.route('/add')
-
-def add():
-    # connect to the database
-
-    # insert new data
-
-    # return a message to the user
-    return ""
+def login():
+    if request.method == 'POST':
+        users = mongo.db.project_users
+        #use the username to find the account
+        existing_user = users.find_one({"username":request.form["username"]})
+        if existing_user:
+            #check if the password is right
+            if existing_user['password'] == request.form["password"] :
+                session['username'] = request.form['username']
+                return redirect(url_for('index'))
+            else:
+                return "Your password doesn't match your username."
+        else:
+            return "There is no user with that username. Try making an account."
+    else:
+        return render_template('login.html', message = '')
